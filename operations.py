6b0516b766c73f5e5ec1323f626a391eea1c0303
AhmadNaderAdel <ahmadnaderx1998@gmail.com>
2020-05-17 17:17:32 +0200

    finished the project

--- operations.py
+++ operations.py
@@ -1,6 +1,10 @@
-#Ahmed A.Allam
+#Ahmed A.Allam & Ahmad Nader Adel & Eslam Wael Reda & Hazem Mohamed & Ahmad Mostafa El Matbaagy
 import numpy as numb
 import math
+
+
+#Multiply MatrixA by MatrixB and return the result
+#Error checking is done in the GUI
 def MatrixMultiplication( MatrixA,MatrixB ):
    MatrixC=[]
    if len(MatrixA[0])==len(MatrixB):
@@ -10,22 +14,11 @@ def MatrixMultiplication( MatrixA,MatrixB ):
         for j in range (len(MatrixB[0])):
             for k in range(len(MatrixA[0])):
                 MatrixC[i][j]=round(MatrixC[i][j]+MatrixA[i][k]*MatrixB[k][j], 4)
-   print(MatrixC)
    return MatrixC
 
-def MatrixMultiplicationNumpy( MatrixA,MatrixB):
-   "This Function Calcualtes Matrix Multiplication of two matrices A and B the matrices are passed as a list of row by row elements if the are not matched it return a null array"
-   MatrixC=None
-   if MatrixA is not None and MatrixB is not None:
-     if len(MatrixA[0])==len(MatrixB):
-       A=numb.array(MatrixA)
-       B=numb.array(MatrixB)
-       MatrixC=A.dot(B)
-   return MatrixC
 
+#This function inverts Matrix A and returns the result
 def MatrixInverse( MatrixA):
-   "This Function Calcualtes Matrix Inverse passed as a list of row by row elements if the are not matched it return a null array"
-
    A=numb.array(MatrixA)
    x=numb.linalg.det(A)
    if (x==0):
@@ -35,6 +28,7 @@ def MatrixInverse( MatrixA):
    return MatrixC
 
 
+#This function checks if the dimension of a matrix is square to make sure we can perform the power method
 def checkSquare(number):
     root = math.sqrt(number)
     if int( root+0.5 ) ** 2 == number:
@@ -42,32 +36,59 @@ def checkSquare(number):
     else:
         return False
 
-#print (MatrixMultiplicationNotNummby([[1 ,0, 2],[2, 5, 6],[3, 7, 30]],[[1,2],[7,10],[9,60]]))
-#print (MatrixMultiplication(MatrixInverse([[1 ,0, 2],[2, 5, 6],[3, 7, 30]]),[[1,2],[7,10],[9,60]]))
-
+#This functions takes the number of iterations, the matrix to calculate eigenpair for, and the initial values
+#It returns EigenVector with the eigenValue appended at the end
 def PowerMethod(noOfIterations, Matrix, initialValues):
          eigenValue = []
          eigenVector = []
          for i in range(noOfIterations):
             eigenVector = MatrixMultiplication(Matrix, initialValues)
-            print("Eigen vector before dividing is: ")
-            print(eigenVector)
             eigenValue = max(eigenVector)
             newEigenVector = [[]]
             newEigenVector[0] = [round(x[0] /  eigenValue[0],4) for x in eigenVector]
             for i in range(len(newEigenVector[0])):
                   initialValues[i] = [newEigenVector[0][i]]
                   
-            print("Eigen vector after dividing is: ")
 
-            print(initialValues)
          initialValues.append(eigenValue)
          return initialValues
-    
-    
-#PowerMethod(3, [[9, -1, 2], [-1,5,0], [2,0,5]], [[3], [0] ,[1]])
-# print(numb.linalg.inv(numb.array([[7.0, 0.0, -2.0], [0.0, 3.0, 0.0], [-2.0, 0.0, 7.0]])))
-# print(MatrixInverse([[7.0, 0.0, -2.0], [0.0, 3.0, 0.0], [-2.0, 0.0, 7.0]]))
-# print(MatrixMultiplication([[0.1556, 0.0       ,  0.0444],
-#  [0.0       ,  0.3333, 0.0        ],
-#  [0.0444 ,0.0        , 0.1556]], [[0.1],[0.9],[0.09]]))
+
+
+#This function returns the tranpose of a matrix
+def transpose(Matrix):
+   return [[Matrix[j][i] for j in range(len(Matrix))] for i in range(len(Matrix[0]))] 
+
+
+#This function prints a matrix for debugging purposes    
+def printMatrix(Matrix):
+   for row in range(len(Matrix)):
+      print(Matrix[row])
+
+#This function returns the difference of 2 matrices
+def subtractMatrix(MatrixA, MatrixB):
+   result = [[0]*len(MatrixA) for i in range(len(MatrixA[0]))];  
+   for row in range(len(MatrixA)):
+      for column in range(len(MatrixA[0])):
+         result[row][column] = MatrixA[row][column] - MatrixB[row][column]
+
+   return result
+
+
+
+#This function calculates the deflated matrix given the original matrix, and the eigen pair
+def DeflatedMatrix(Matrix, eigenVector, eigenValue):
+   result = 0
+   for row in range(len(eigenVector)):
+      for column in range(len(eigenVector[0])):
+         result += eigenVector[row][column] ** 2
+   number = eigenValue/result
+   newMatrix = MatrixMultiplication(eigenVector, transpose(eigenVector))
+   for row in range(len(newMatrix)):
+      for column in range(len(newMatrix[0])):
+         newMatrix[row][column] *= number
+   return subtractMatrix(Matrix, newMatrix)
+
+
+
+
+
